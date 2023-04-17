f1b725a2a22c2f8b1b9fd1a3e2cf2e787410892a
bdistin <bdistin@gmail.com>
2023-04-17 23:02:20 +0100

    Autocommit from 2023-04-17 23:02:20
    Klipper on commit: eccd9a9
    
    Moonraker on commit: 5a76b5b
    
    Mainsail version: v2.5.1

--- scripts/patch-include.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-filename="/home/pi/klipper/klippy/configfile.py"
-search="glob\.glob\(include_glob\)"
-replace="glob.glob(include_glob, recursive=True)"
-
-grep -q $search $filename &&
-    sed -i "s/$search/$replace/" $filename &&
-    echo RESTART > /tmp/printer
