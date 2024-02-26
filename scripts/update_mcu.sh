36ed00794a5cbde31da050f7c10825c71e6d4744
bdistin <bdistin@gmail.com>
2024-02-26 15:39:26 -0600

    Autocommit from 2024-02-26 15:39:26
    Klipper on commit: 9f41f53
    
    Moonraker on commit: 71b46cc
    
    Mainsail version: v2.10.0

--- scripts/update_mcu.sh
+++ scripts/update_mcu.sh
@@ -1,40 +1,95 @@
+#!/usr/bin/env bash
+
+###########################################################################################
+### Please set the following paths accordingly.                                         ###
+###########################################################################################
+
+### Path to your Klipper folder, by default that is '~/klipper'
+klipper_folder=~/klipper
+
+### Path to your Katapult folder, by default that is '~/katapult'
+katapult_folder=~/katapult
+
+### Path to your mcu config folder
+config_folder=~/printer_data/config/scripts/mcu_configs
+
+### Path to your firmware folder, by default, will be '~/firmware'
+firmware_folder=~/firmware
+
+###########################################################################################
+### List your MCUs in the following format:                                             ###
+###                                                                                     ###
+### declare -A mcu0=( # The number after mcu must go in order starting at 0             ###
+###    [type]='kraken' # Required                                                       ###
+###    [can_address]='5cbcb7c7dc7f' # Optional                                          ###
+###    [usb_address]='usb-CanBoot_stm32f446xx_4A0021000651303431333234-if00' # Optional ###
+### )                                                                                   ###
+###########################################################################################
+
+declare -A mcu0=(
+    [type]='sb2209'
+    [can_address]='a6dbb36fd844'
+)
+
+declare -A mcu1=(
+    [type]='mmu'
+    [can_address]='1e67a8e2112e'
+)
+
+declare -A mcu2=(
+    [type]='kraken'
+    [can_address]='5cbcb7c7dc7f'
+    [usb_address]='usb-CanBoot_stm32f446xx_4A0021000651303431333234-if00'
+)
+
+declare -A mcu3=(
+    [type]='rpi'
+)
+
+###########################################################################################
+########################### !!! DO NOT EDIT BELOW THIS LINE !!! ###########################
+###########################################################################################
+
+declare -n mcu
+
 sudo service klipper stop
 
-cd ~/klipper
+cd $klipper_folder
 git pull --autostash
 
-cp -f ~/printer_data/config/scripts/mcu_configs/{config.octopus,config.sb2040,config.rpi} ~/klipper/
+for mcu in ${!mcu@}; do
+  cp -f ${config_folder}/config.${mcu.type} $klipper_folder
 
-make clean KCONFIG_CONFIG=config.octopus
-make menuconfig KCONFIG_CONFIG=config.octopus
-make KCONFIG_CONFIG=config.octopus
-mv ~/klipper/out/klipper.bin ~/firmware/octopus_1.1_klipper.bin
-read -p "Octopus firmware built, please check above for any errors. Press [Enter] to continue building firmware, or [Ctrl+C] to abort"
+  make clean KCONFIG_CONFIG=config.${mcu.type}
+  make menuconfig KCONFIG_CONFIG=config.${mcu.type}
 
-make clean KCONFIG_CONFIG=config.sb2040
-make menuconfig KCONFIG_CONFIG=config.sb2040
-make KCONFIG_CONFIG=config.sb2040
-mv ~/klipper/out/klipper.bin ~/firmware/sb2040_1.0_klipper.bin
-read -p "SB2040 firmware built, please check above for any errors. Press [Enter] to continue building firmware, or [Ctrl+C] to abort"
+  if [[ -n "${mcu.can_address}" ]]; then
+    make KCONFIG_CONFIG=config.${mcu.type}
+    mv ${klipper_folder}/out/klipper.bin ${mcu.type}/${mcu.type}_klipper.bin
 
-make clean KCONFIG_CONFIG=config.rpi
-make menuconfig KCONFIG_CONFIG=config.rpi
-make KCONFIG_CONFIG=config.rpi
-read -p "RPi firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
+    read -p "${mcu.type} firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
 
-cd ~/CanBoot/scripts
-python3 flash_can.py -i can0 -u 9d488cc48530 -f ~/firmware/sb2040_1.0_klipper.bin
-read -p "SB2040 firmware flashed, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
-
-python3 flash_can.py -i can0 -u dd8d9d6ff9fd -f ~/firmware/octopus_1.1_klipper.bin
-read -p "We intentionally caused an error on the Octopus to boot into the bootloader. Please ignore and Press [Enter] to continue flashing, or [Ctrl+C] to abort"
-python3 flash_can.py -d /dev/serial/by-id/usb-CanBoot_stm32f446xx_4A0021000651303431333234-if00 -f ~/firmware/octopus_1.1_klipper.bin
-read -p "Octopus firmware flashed, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
-
-cd ~/klipper
-make flash KCONFIG_CONFIG=config.rpi
-read -p "RPi firmware flashed, please check above for any errors. Press [Enter] to finish up, or [Ctrl+C] to abort"
-
-cp -f ~/klipper/{config.octopus,config.sb2040,config.rpi} ~/printer_data/config/scripts/mcu_configs/
+    python3 ${katapult_folder}/scripts/flash_can.py -i can0 -u ${mcu.can_address} -f ${firmware_folder}/${mcu.type}_klipper.bin
+    
+    if [[ -n "${mcu.usb_address}" ]]; then
+      read -p "We intentionally caused an error on the ${mcu.type} to boot into the bootloader. Please ignore and Press [Enter] to continue flashing, or [Ctrl+C] to abort"
+      
+      python3 flash_can.py -d /dev/serial/by-id/${mcu.usb_address} -f ${firmware_folder}/${mcu.type}_klipper.bin
+    fi
+    
+    read -p "${mcu.type} firmware flashed, please check above for any errors. Press [Enter] to continue building, or [Ctrl+C] to abort"
+  elif [[ "${mcu.type" == "rpi" ]]; then
+    make flash KCONFIG_CONFIG=config.${mcu.type}
+    
+    read -p "${mcu.type} firmware flashed, please check above for any errors. Press [Enter] to continue building, or [Ctrl+C] to abort"
+  else
+    make KCONFIG_CONFIG=config.${mcu.type}
+    mv ${klipper_folder}/out/klipper.bin ${firmware_folder}/${mcu.type}_klipper.bin
+    
+    read -p "${mcu.type} firmware built, please check above for any errors. Firmware is stored here: ${firmware_folder}/${mcu.type}_klipper.bin and you will need to install it per your board type manually. Press [Enter] to continue, or [Ctrl+C] to abort"
+  fi
+  
+  cp -f ${klipper_folder}/config.${mcu.type} $config_folder
+done
 
 sudo service klipper start
